0590d5230bd3463e92a462011e15713e36a1e211
festus <festuschiagozie169@gmail.com>
2024-09-08 20:42:59 +0100

    first commit

---
+++ nekky.js
@@ -0,0 +1,8 @@
+const menList = document.querySelectorAll(".navBar ul li");
+/*
+for(men of menList){
+    men.addEventListener("hover", function(){
+        this.style.background = "rgb(142, 168, 9);"
+    })
+}
+*/
